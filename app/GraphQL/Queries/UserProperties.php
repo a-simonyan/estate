ac15436ab74b2d59902172ae8d48ff154fea1455
user <mihran.baldryan94@gmail.com>
2021-04-26 17:55:42 +0400

    user properties by status

--- app/GraphQL/Queries/UserProperties.php
+++ app/GraphQL/Queries/UserProperties.php
@@ -15,8 +15,26 @@ class UserProperties
     {
         $user_auth   = Auth::user();
         $user_id     = $user_auth->id;
+       
+        $is_public_status = !empty($args['is_public_status'])? $args['is_public_status']:'published';
+        $first = !empty($args['first']) ? $args['first'] : 10;
+        $page  = !empty($args['page']) ? $args['page'] : 1;
 
-        $properties = Property::where('user_id',$user_id)->where('is_delete', false)->orderBy('created_at', 'DESC')->get();
+        $field = 'id';
+        // ASC or DESC
+        $order = 'DESC';
+
+        if(!empty($args['orderBy'])){
+            $field = $args['orderBy']['field'];
+            $order = $args['orderBy']['order'];
+        };
+
+        $properties = Property::where('user_id',$user_id)
+                               ->where('is_delete', false)
+                               ->where('is_save', false)
+                               ->where('is_public_status', $is_public_status)
+                               ->orderBy($field, $order)
+                               ->paginate($first,['*'],'page', $page);
 
         return $properties;
     }
--- app/GraphQL/Queries/UserSaveProperties.php
+++ app/GraphQL/Queries/UserSaveProperties.php
@@ -15,9 +15,23 @@ class UserSaveProperties
         $user_auth   = Auth::user();
         $user_id     = $user_auth->id;
 
-        $properties = Property::where('user_id',$user_id)->where('is_delete', false)
-                              ->where('is_save',true)->orderBy('created_at', 'DESC')
-                              ->get();
+        $first = !empty($args['first']) ? $args['first'] : 10;
+        $page  = !empty($args['page']) ? $args['page'] : 1;
+
+        $field = 'id';
+        // ASC or DESC
+        $order = 'DESC';
+
+        if(!empty($args['orderBy'])){
+            $field = $args['orderBy']['field'];
+            $order = $args['orderBy']['order'];
+        };
+
+        $properties = Property::where('user_id',$user_id)
+                              ->where('is_delete', false)
+                              ->where('is_save',true)
+                              ->orderBy($field, $order)
+                              ->paginate($first,['*'],'page', $page);
 
         return $properties;
     }
