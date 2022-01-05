a71a2ce9cf748b14fbfe77195f56db8794e90891
user <mihran.baldryan94@gmail.com>
2022-01-05 15:40:20 +0400

    add UserProperty query

---
+++ app/GraphQL/Queries/Property.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Property as PropertyModel;
+
+class Property
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $property_id = $args['id'];
+        $property = PropertyModel::Find($property_id);                            
+
+        if($property&&$property->is_public_status == 'published'){
+            return $property;
+        } 
+
+        return null;
+    }
+}
