483f9b6c8d6bd343b171fc3bd5beaae330e3c172
user <mihran.baldryan94@gmail.com>
2022-01-12 19:49:18 +0400

    recaptcha id

--- config/googlerecapcha.php
+++ config/googlerecapcha.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'recapcha_secret_web' =>'6LfUG88ZAAAAANZroksBgRyufM49HHJ2T_h5ZlmA',
+    'recapcha_secret_web' =>'6LdHqAYeAAAAALfhj1FaJV-lfzFz-JLbZDK_X8NC',
 
-    'recapcha_secret_androud' =>'6LdIHMYZAAAAABXWnWD6jiYGs9Nm9bU3If32pZTS',
+    'recapcha_secret_androud' =>'6LdHqAYeAAAAALfhj1FaJV-lfzFz-JLbZDK_X8NC',
 
-    'recapcha_secret_ios' =>'6LdIHMYZAAAAABXWnWD6jiYGs9Nm9bU3If32pZTS',
+    'recapcha_secret_ios' =>'6LdHqAYeAAAAALfhj1FaJV-lfzFz-JLbZDK_X8NC',
 
 ];
