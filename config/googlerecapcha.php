c53fa30ece9a2a6426933514cfff0f26c31c3cf7
user <mihran.baldryan94@gmail.com>
2022-01-13 14:02:00 +0400

    add googlerecapcha

--- config/googlerecapcha.php
+++ config/googlerecapcha.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'recapcha_secret_web' =>'6LdHqAYeAAAAALfhj1FaJV-lfzFz-JLbZDK_X8NC',
+    'recapcha_secret_web' =>'6LdHqAYeAAAAAORAKthZ1WAS0m1kf2fggAw7Uf3C',
 
-    'recapcha_secret_androud' =>'6LdHqAYeAAAAALfhj1FaJV-lfzFz-JLbZDK_X8NC',
+    'recapcha_secret_androud' =>'6LdHqAYeAAAAAORAKthZ1WAS0m1kf2fggAw7Uf3C',
 
-    'recapcha_secret_ios' =>'6LdHqAYeAAAAALfhj1FaJV-lfzFz-JLbZDK_X8NC',
+    'recapcha_secret_ios' =>'6LdHqAYeAAAAAORAKthZ1WAS0m1kf2fggAw7Uf3C',
 
 ];
