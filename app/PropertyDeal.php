d2d5f0319b384e96d8aad7f823fac5eda56cb629
user <mihran.baldryan94@gmail.com>
2022-02-18 18:42:09 +0400

    add usd price in  property

--- app/PropertyDeal.php
+++ app/PropertyDeal.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\CurrencyType;
 
 class PropertyDeal extends Model
 {
+
     public $timestamps = false;
     protected $table ="property_deals";
     protected $fillable= ['property_id','deal_type_id','price','currency_type_id'];
@@ -18,5 +20,16 @@ class PropertyDeal extends Model
     }
     public function currency_type(){
         return $this->belongsTo('App\CurrencyType','currency_type_id','id');
+    }
+    public function getPriceUsdAttribute(){
+          $usd =  CurrencyType::where('name','USD')->first(['id','rate']);
+          if($usd&&$usd->id == $this->currency_type_id){
+             return $this->price;
+          } else {
+            
+            $currencyType = CurrencyType::where('id',$this->currency_type_id)->first(['rate']);
+
+            return round(($this->price*$currencyType->rate)/$usd->rate, 2);
+          }
     }
 }
