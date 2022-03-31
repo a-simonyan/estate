61b2f3ac5692076bf62b3621608cc51a6665e8af
user <mihran.baldryan94@gmail.com>
2022-03-31 17:21:33 +0400

    add property image validate

--- app/GraphQL/Directives/CreatePropertyValidationDirective.php
+++ app/GraphQL/Directives/CreatePropertyValidationDirective.php
@@ -12,14 +12,14 @@ class CreatePropertyValidationDirective extends ValidationDirective
         
         return [
             'property_type'     => ['required','string','in:apartment,mansion,land_area,commercial_area'],
-            'deal_type'         => ['string','in:good,average,poor','nullable'],
+            'deal_type'         => ['string','in:good,average,poor,renovated,zero_condition','nullable'],
             'bulding_type_id'   => [],
             'land_area_type_id' => [],
             'latitude'          => ['required','numeric'],
             'longitude'         => ['required','numeric'],
             'address'           => ['required','string'],
-            'property_images.*' => ['image','mimes:jpeg,jpg,png,svg,gif','nullable'],
-            'property_images.0' => ['required','image','mimes:jpeg,jpg,png,svg,gif'],
+            'property_images.*' => ['image','max:2048','mimes:jpeg,jpg,png,svg,gif','nullable']
+            // 'property_images.0' => ['required','image','mimes:jpeg,jpg,png,svg,gif'],
         ];
     }
 
