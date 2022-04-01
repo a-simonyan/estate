87d29b822a1cb464598d408e88078a329efe7569
user <mihran.baldryan94@gmail.com>
2022-04-01 11:10:05 +0400

    fix email

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -32,20 +32,18 @@ width: 100% !important;
 <!-- Email Body -->
 <tr>
 <td class="body" width="100%" cellpadding="0" cellspacing="0">
-<table class="inner-body" align="center" width="670" cellpadding="0" cellspacing="0" role="presentation">
+<div class="inner-body" align="center" cellpadding="0" cellspacing="0" role="presentation">
     <div class="email-top-title"><h2>{{__('email.welcome')}}</h2></div>
-    <div class="box-inline-block box-width" >
+    <div class="box-inline-block box-width-100" >
             <img class="email-logo" src="https://myestate.am:446/storage/property/XSA7CrVSzv1645541083.jpg">
     </div>
 <!-- Body content -->
-<tr>
-<td class="content-cell">
+
 {{ Illuminate\Mail\Markdown::parse($slot) }}
 
 {{ $subcopy ?? '' }}
-</td>
-</tr>
-</table>
+
+</div>
 </td>
 </tr>
 
