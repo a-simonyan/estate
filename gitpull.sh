6e81c6c34d4b02451d8ab4e4e7ac398af71d545e
Sargis Davtyan <sargis.davtyan83@gmail.com>
2022-04-20 05:26:35 -0700

    test

--- gitpull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /var/www/html
-git clean -f
-git checkout -- $(git ls-files -m)
-git pull "https://arnologyllc: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@github.com/arnologyllc/estate.git" devmaster
-yes | composer install
-php artisan migrate
-php artisan config:clear
