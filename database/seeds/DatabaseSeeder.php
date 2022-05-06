8358a71dc2414bd59d4f971c26bb3e791a41d480
user <user@user.com>
2022-05-06 12:09:06 +0400

    add property views statistics

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {   
 
-         $this->call(PropertySeeder::class);
+        // $this->call(PropertySeeder::class);
         // $this->call(UserTypeSeeder::class);
         // $this->call(UserSeeder::class);
         // $this->call(LanguageSeeder::class);
