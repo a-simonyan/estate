63d741f906dae6d2ef8e605481bb8ecfcb273f05
user <user@user.com>
2022-07-05 15:48:45 +0400

    add admin config

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/mail', function () {
-    dd("admin spam");
+    dd("admin config");
     $date = Carbon::now()->subYear()->format('Y-m-d');
 //    $properties = Property::whereNull('deleted_at')
 //        ->whereNull('archived_at')
