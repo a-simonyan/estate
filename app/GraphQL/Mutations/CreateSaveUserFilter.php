1e4cce347879069e2ae35fe0866d088d0bb397f6
user <user@user.com>
2022-07-07 15:11:09 +0400

    fix save filters schema

--- app/GraphQL/Mutations/CreateSaveUserFilter.php
+++ app/GraphQL/Mutations/CreateSaveUserFilter.php
@@ -13,17 +13,18 @@ class CreateSaveUserFilter
      */
     public function __invoke($_, array $args)
     {
-        $user_id = Auth::user()->id;
-        $json_properties_filters = json_encode($args);
-     
-        $saveUserFilter = SaveUserFilter::create([
-                                          'properties_filters' => $json_properties_filters,
-                                          'user_id' => $user_id
-                                         ]);
-        if($saveUserFilter){
-           return ['status' => true ]; 
-        };
-        
+        if(count($args)) {
+            $user_id = Auth::user()->id;
+            $json_properties_filters = json_encode($args);
+
+            $saveUserFilter = SaveUserFilter::create([
+                'properties_filters' => $json_properties_filters,
+                'user_id' => $user_id
+            ]);
+            if ($saveUserFilter) {
+                return ['status' => true];
+            };
+        }
         return ['status' => false ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/mail', function () {
-    dd("admin filter rollback id for test ");
+    dd("fix save filter schema");
     $date = Carbon::now()->subYear()->format('Y-m-d');
 //    $properties = Property::whereNull('deleted_at')
 //        ->whereNull('archived_at')
